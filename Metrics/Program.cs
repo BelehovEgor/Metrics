46c6d5a42ba4043a8537c5bf2e56aefe4e2d9de6
BelehovEgor <belehov.egor2001@yandex.ru>
2023-11-05 14:57:15 +0300

    added l&k metrics

---
+++ Metrics/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json;
+using Metrics;
+
+var (classes, interfaces) = SolutionReader.GetClassesAndInterfaces("D:\\itmo\\verify\\CsharpSSA");
+
+var classesInfo = LKMetricsCounter.Get(classes, interfaces).ToList();
+
+await using StreamWriter outputFile = new StreamWriter("CsharpSSA_L&KMetrics.txt");
+
+await outputFile.WriteAsync(JsonSerializer.Serialize(classesInfo));
